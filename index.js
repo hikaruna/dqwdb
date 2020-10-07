a7e028ab8c3ba3442eb457ecf510745425f8c50a
hikaruna <hikaruna@users.noreply.github.com>
2020-10-08 04:34:19 +0900

    extract index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+﻿import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/vue.esm.browser.js';
+import { main } from './main.js';
+
+(async () => {
+
+    const result = await main();
+
+    const app = new Vue({
+        el: '#app',
+        data: {
+            minds: result
+        }
+    });
+})();
