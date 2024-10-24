1ff3409c635643d769cdc790ece5e2e468f05c88
hikaruna <169936+hikaruna@users.noreply.github.com>
2024-10-25 00:57:12 +0900

    装備のダメージアップ特殊交換マトリクスを作りたかった

--- data.sqlite3.schema.sql
+++ data.sqlite3.schema.sql
@@ -94,3 +94,57 @@ CREATE TABLE IF NOT EXISTS "kokoro_grade" (
 	"order"	INTEGER NOT NULL UNIQUE,
 	PRIMARY KEY("kokoro_grade")
 );
+CREATE VIRTUAL TABLE v_item USING pivot_vtab (
+  -- ROWS
+  (SELECT DISTINCT item from item),
+  (SELECT DISTINCT zokusei, zokusei FROM zokusei),
+  (SELECT 'null')
+);
+CREATE TABLE sqlean_define(name text primary key, type text, body text);
+CREATE VIEW v_item_skill_tokusyukouka AS
+  SELECT * FROM item
+  JOIN item_skill_tokusyukouka
+  ON item.item = item_skill_tokusyukouka.item
+/* v_item_skill_tokusyukouka(item,"order",type,rarity,"item:1",skill_tokusyukouka,hosei_per,hosei_value,kougekizi_no_kakuritu_hosei,hanekaesu_kakuritu_hosei,tern_kaisizi_sentou_syuuryouzi,condition,genkaitoppa,rensei) */;
+CREATE TABLE IF NOT EXISTS "skill_type" (
+	"skill_type"	TEXT NOT NULL,
+	PRIMARY KEY("skill_type")
+);
+CREATE TABLE IF NOT EXISTS "tokusyukouka_target_skill_type" (
+	"tokusyukouka_target_skill_type"	TEXT NOT NULL,
+	"skill_type"	TEXT NOT NULL,
+	PRIMARY KEY("tokusyukouka_target_skill_type"),
+	FOREIGN KEY("skill_type") REFERENCES "skill_type"("skill_type") ON UPDATE RESTRICT ON DELETE RESTRICT
+);
+CREATE TABLE IF NOT EXISTS "zokusei_type" (
+	"zokusei_type"	TEXT NOT NULL,
+	PRIMARY KEY("zokusei_type")
+);
+CREATE TABLE IF NOT EXISTS "tokusyukouka_target_zokusei_type" (
+	"tokusyukouka_target_zokusei_type"	TEXT NOT NULL,
+	"zokusei_type"	TEXT NOT NULL UNIQUE,
+	PRIMARY KEY("tokusyukouka_target_zokusei_type"),
+	FOREIGN KEY("zokusei_type") REFERENCES "zokusei_type"("zokusei_type") ON UPDATE RESTRICT ON DELETE RESTRICT
+);
+CREATE TABLE IF NOT EXISTS "zokusei_type_zokusei" (
+	"zokusei_type"	TEXT NOT NULL,
+	"zokusei"	TEXT NOT NULL,
+	PRIMARY KEY("zokusei_type","zokusei"),
+	FOREIGN KEY("zokusei") REFERENCES "zokusei"("zokusei") ON UPDATE RESTRICT ON DELETE RESTRICT,
+	FOREIGN KEY("zokusei_type") REFERENCES "zokusei_type"("zokusei_type") ON UPDATE RESTRICT ON DELETE RESTRICT
+);
+CREATE VIEW damage_hosei_tokusyukouka_zokusei AS
+SELECT
+  (
+    (
+	  CASE
+		WHEN tokusyukouka_target_zokusei_type <> '' THEN tokusyukouka_target_zokusei_type||'属性'
+		ELSE ''
+	  END
+	)||tokusyukouka_target_skill_type.tokusyukouka_target_skill_type||'ダメージ') as tokusyukouka
+  ,zokusei_type_zokusei.zokusei
+FROM tokusyukouka_target_zokusei_type
+CROSS JOIN tokusyukouka_target_skill_type
+LEFT OUTER JOIN zokusei_type_zokusei ON tokusyukouka_target_zokusei_type.zokusei_type = zokusei_type_zokusei.zokusei_type
+WHERE tokusyukouka_target_zokusei_type <> '' OR tokusyukouka_target_skill_type <> ''
+/* damage_hosei_tokusyukouka_zokusei(tokusyukouka,zokusei) */;
--- sqlite2csv.sh
+++ sqlite2csv.sh
@@ -7,6 +7,7 @@ TS=`sqlite3 $1 "SELECT tbl_name FROM sqlite_master WHERE type='table' and tbl_na
 for T in $TS; do
 
 sqlite3 $1 <<!
+.load ./pivotvtab 
 .headers on
 .mode csv
 .output data.sqlite3.$T.csv
